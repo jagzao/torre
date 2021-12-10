1b4a9fa267db9c730e679a974844fecd1173e945
Juma <academiajuma@gmail.com>
2021-12-10 17:20:42 -0600

    final changes

--- components/User.vue
+++ components/User.vue
@@ -1,20 +1,176 @@
 <template>
     <div>
-        <p>foto</p>
-        <p>nombre</p>
+    <b-card-group deck>
+        <b-card 
+            bg-variant="dark" 
+            text-variant="white" 
+            class="text-center">
+        <div>
+            <h3 class="m-5">{{person.name}}</h3>
+            <b-img 
+                center
+                thumbnail 
+                :src="person.picture"
+                class="photo m-4"
+                alt="Circle image"></b-img>
 
-        <h3>Skills and interests:</h3>
-        
-        <h4>Master / Influencer:</h4>
-        <b-button 
-            pill 
-            variant="secondary"
-            v-for="(skill, index) in SkillsMaster"
-            :key="index">
-            Secondary
-        </b-button>
+            <h4>
+                Skills and interests:
+            </h4>
+            <hr>
+            
+            <h4>Master / Influencer:</h4>
+            <b-button 
+                pill 
+                class="m-1"
+                variant="secondary"
 
-        <h4>Proficient</h4>
+                v-for="(skill, index) in SkillsMaster"
+                :key="index">
+                {{skill.name}}
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="skill.value"></b-rating>
+            </b-button>
+
+            <hr>
+
+            <h4>Expert</h4>
+            <b-button 
+                pill
+                class="m-1"
+                variant="secondary"
+                v-for="(skill, index) in SkillsExpert"
+                :key="index">
+                {{skill.name}}
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="skill.value"></b-rating>
+            </b-button>
+            <b-button @click="modalShow = !modalShow">Open Modal</b-button>
+            <b-modal v-model="modalShow">Hello From Modal!</b-modal>
+            <hr>
+
+            <h4>Proficient</h4>
+            <b-button 
+                pill 
+                class="m-1"
+                variant="secondary"
+                v-for="(skill, index) in SkillsProfiencie"
+                :key="index">
+                {{skill.name}}
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="skill.value"></b-rating>
+            </b-button>
+            <hr>
+
+            <h4>Novice</h4>
+            <b-button 
+                pill 
+                class="m-1"
+                variant="secondary"
+                v-for="(skill, index) in SkillsNovice"
+                :key="index">
+                {{skill.name}}
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="skill.value"></b-rating>
+            </b-button>
+            <hr>
+
+            <h4>No experience, but interests</h4>
+            <b-button 
+                pill 
+                class="m-1"
+                variant="secondary"
+                v-for="(skill, index) in SkillsNoExperience"
+                :key="index">
+                {{skill.name}}
+                
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="skill.value"></b-rating>
+            </b-button>
+            <hr>
+
+            <h4>Recommendations {{recommendations}}</h4>
+
+            <h4>Languages</h4>
+            <b-button 
+                pill 
+                class="m-1"
+                variant="secondary"
+                v-for="(lang, index) in Languages"
+                :key="index">
+                {{lang.language}} 
+                <b-rating 
+                    variant="warning"
+                    inline 
+                    no-border
+                    readonly
+                    v-model="lang.value"></b-rating>
+            </b-button>
+
+            <hr>
+
+            <b-container class="bv-example-row">
+                <b-row>
+                    <div v-for="(xp, index) in Experiences" :key="index">
+                        <b-col v-if="index % 2 === 0" class="m-2">
+                            <p class="exp_title">{{xp.position}}</p>
+                            <p class="exp_text">{{xp.company}}</p>
+                            <p class="exp_text">
+                                {{xp.from}} - {{xp.to}}
+                            </p>
+                        </b-col>
+                        <b-col v-else class="m-2">
+                            <p class="exp_title">{{xp.position}}</p>
+                            <p class="exp_text">{{xp.company}}</p>
+                            <p class="exp_text">
+                                {{xp.from}} - {{xp.to}}
+                            </p>
+                        </b-col>
+                    </div>
+                </b-row>
+            </b-container>
+            
+            <hr>
+            <div v-show="false">
+                <h4>Other people with this skill</h4>
+                <ul class="list-unstyled">
+                    <b-media tag="li" v-for="(item, index) in Others" :key="index">
+                        <template #aside>
+                            <b-img blank 
+                                thumbnail 
+                                :src="person.picture"
+                                blank-color="#abc" width="64" alt="placeholder"></b-img>
+                        </template>
+                        <h5 class="mt-0 mb-1">{{item.name}}</h5>
+                        <p class="mb-0">
+                            {{item.professionalHeadline}}
+                        </p>
+                    </b-media>
+                </ul>
+            </div>
+        </div>
+    </b-card>
+    </b-card-group>
     </div>
 </template>
 <script>
@@ -22,10 +178,16 @@ import datos from '/static/test.json'
 export default{
     data() {
         return {
-            user:{},
+            person:{},
             SkillsMaster: [],
+            SkillsExpert: [],
             SkillsProfiencie: [],
-            SkillsMaster: [],
+            SkillsNovice: [],
+            SkillsNoExperience: [],
+            Languages: [],
+            recommendations: 0,
+            Experiences: [],
+            Others: []
         }
     },
     created() {
@@ -33,22 +195,76 @@ export default{
     },
     methods: {
         Default() {
-            console.log(datos)
             const options = {
                 method: 'GET', 
-                url: 'https://bio.torre.co/api/bios/jagzao'
+                url: 'https://bio.torre.co/api/bios/jagzao',
+                headers: { 'Access-Control-Allow-Origin': '*' },
             };
-            this.$axios.request(options)
-                .then(function (response) {
-                    // console.log(response.data);
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-        },
-        GetSkills() {
+            this.person = datos.person
+            this.SkillsMaster = this.SetData(datos, 'master')
+            this.SkillsExpert = this.SetData(datos, 'expert')
+            this.SkillsProfiencie = this.SetData(datos, 'proficient')
+            this.SkillsNovice = this.SetData(datos, 'novice')
+            this.SkillsNoExperience = this.SetData(datos, 'no-experience-interested')
+            this.Languages = datos.languages.map(s => {
+                return {
+                    language: s.language,
+                    value: this.getLangValue(s.fluency)
+                }
+            })
+            this.Experiences = datos.jobs.map(s => {
+                return {
+                    position: s.name,
+                    company: s.organizations[0].name,
+                    from: s.fromMonth !== undefined ? `${s.fromMonth} ${s.fromYear}` : "",
+                    to: s.toMonth !== undefined ? `${s.toMonth} ${s.toYear}` : ""
+                }
+            })
+            this.Others = []
 
+            // this.$axios.request(options)
+            //     .then(function (response) {
+            //         // console.log(response.data);
+            //     })
+            //     .catch(function (error) {
+            //         console.error(error);
+            //     });
+        },
+        SetData(info, level) {
+            let filter = info.strengths
+                .filter(s => s.proficiency === level)
+                .map((s, index) => {
+                    return {
+                        proficiency: s.proficiency,
+                        name: s.name,
+                        recommendations: s.recommendations,
+                        value: index
+                    }
+                })
+            return filter
+        },
+        getLangValue(lang) {
+            switch(lang) {
+                case 'fully-fluent': return 5
+                default: return 1
+            }
         }
     }
 }
 </script>
+<style>
+.photo {
+    height: 160px;
+    width: 160px;
+    clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+}
+.form-control {
+    background-color: transparent !important;
+}
+.exp_title {
+    color: #CDDC39;
+}
+p {
+    margin: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Torre</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar 
+      toggleable="lg" type="dark" 
+      variant="dark">
+      <b-navbar-toggle target="nav-collapse">
+      </b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">User</b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search">
-              <b-iconstack font-scale="5">
-                <b-icon stacked icon="circle-fill" variant="info"></b-icon>
-                <b-icon stacked icon="search-fill" scale="0.5" variant="white"></b-icon>
-                <b-icon stacked icon="circle" variant="danger"></b-icon>
-              </b-iconstack>
-            </b-form-input>
-          </b-nav-form>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
+          
         </b-navbar-nav>
       </b-collapse>
+      <b-navbar-brand href="#">torre.co</b-navbar-brand>
+      <b-nav-form>
+        <b-form-input size="sm" class="mr-sm-2" placeholder="Search">
+          <b-iconstack font-scale="5">
+            <b-icon stacked icon="circle-fill" variant="info"></b-icon>
+            <b-icon stacked icon="search-fill" scale="0.5" variant="white"></b-icon>
+            <b-icon stacked icon="circle" variant="danger"></b-icon>
+          </b-iconstack>
+        </b-form-input>
+      </b-nav-form>
+      <b-button size="sm" class="my-2 my-sm-0" 
+        type="submit">
+        Login
+      </b-button>
     </b-navbar>
     <User />
 
